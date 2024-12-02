8b234df8d79b55aaa2f9f461b8130e0c31601d0e
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-12-02 11:33:41 +0100

    Add project files.

---
+++ PokemonTest/Pokemon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTest
+{
+    internal class Pokemon
+    {
+        private string Name { get; set; }
+        public int Health;
+        public int Strength;
+        public string Type;
+
+        public Pokemon(string name, int health, int strength, string type)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Type = type;
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Health);
+            Console.WriteLine(Strength);
+            Console.WriteLine(Type);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ PokemonTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using PokemonTest;
+
+var pokemon1 = new Pokemon("Pikachu", 100, 75, "Electric");
+var pokemon2 = new Pokemon("Bulbasaur", 150, 125, "Grass");
+
+Console.WriteLine(pokemon1.GetName());
+Console.WriteLine(pokemon2.GetName());
+pokemon1.PrintStats();
+pokemon2.PrintStats();
